eb5c2d09b9df3b2d433744f966c75cfc9ed662dc
chri5n85@stud.kea.dk <chri5n85@stud.kea.dk>
2017-01-18 02:38:38 +0100

    First commit

---
+++ BinaryTree/ITree.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+    interface ITree<T>
+    {
+        /// <summary>
+        /// Inserts an item of type T into the tree
+        /// </summary>
+        /// <param name="item"></param>
+        void Insert(T item);
+    }
+}
---
+++ BinaryTree/Tree.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+    public class Tree<T> : ITree<T> where T : IComparable<T>
+    {
+        public T NodeData { get; set; }
+        public Tree<T> LeftTree { get; set; }
+        public Tree<T> RightTree { get; set; }
+
+        public Tree(T nodeValue)
+        {
+            this.NodeData = nodeValue;
+            this.LeftTree = null;
+            this.RightTree = null;         
+        }
+
+
+        public void Insert(T item)
+        {
+            T currentNodeValue = this.NodeData;
+
+            if (currentNodeValue.CompareTo(item) > 0)
+            {
+                //Insert into the left subtree
+                if(this.LeftTree == null)
+                {
+                    this.LeftTree = new Tree<T>(item);
+                }
+                else
+                {
+                    this.LeftTree.Insert(item);
+                }
+            }
+            else
+            {
+                //Insert into the right subtree
+                if(this.RightTree == null)
+                {
+                    this.RightTree = new Tree<T>(item);
+                }
+                else
+                {
+                    this.RightTree.Insert(item);
+                }
+            }
+        }
+
+    }
+}
